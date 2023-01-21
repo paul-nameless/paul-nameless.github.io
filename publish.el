368588ceda5818f5af475603e1ead3e7d2214cc9
Paul Nameless <reacsdas@gmail.com>
2023-01-21 10:55:25 +0100

    Remove subscribe form

--- publish.el
+++ publish.el
@@ -12,12 +12,6 @@
   "<hr>
 To get in touch, email me at <a href=\"mailto:reacsdas@gmail.com\">reacsdas@gmail.com</a> <br /><br />
 Discuss and comment in <a href=\"https://t.me/reacsdas\">Telegram</a> | <a href=\"https://github.com/paul-nameless\">Github</a> | <a href=\"https://twitter.com/paul_nameless\">Twitter</a><br/>
-<br/>
-<form action=\"https://42emails.com/api/email\">
-  <input type=\"text\" name=\"email\" placeholder=\"Email\" />
-  <input type=\"hidden\" name=\"form_id\" value=\"c88adf06aac24ae5b6798b33a1075330\" />
-  <input type=\"submit\" value=\"Subscribe\" /><br/>
-</form>
 
 <!-- Cloudflare Web Analytics -->
 <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{\"token\": \"2c0ad197c226461eaccf6861b45f1cf8\"}'>
